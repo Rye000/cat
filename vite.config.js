1105d24e79f4b293a891071cfc87ee2ee8f55197
Rye000 <wei10012000@gmail.com>
2025-11-24 16:39:14 +0800

    測試

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        port: 3001, // 將 3001 替換為你想要的埠號
-        host: "localhost", // 可選，默認為 'localhost'
-    },
+    base: "/my-react-app/",
+    // server: {
+    //     port: 3001, // 將 3001 替換為你想要的埠號
+    //     host: "localhost", // 可選，默認為 'localhost'
+    // },
 });
